fec63dd8337263edaac3c258e020dd878215e049
LorenaMendes <lorenamendes.contato@gmail.com>
2020-05-19 14:54:08 -0300

    model Crawl Requests added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class CrawlRequest(models.Model):
+    source_name = models.CharField(max_length=200)
+    base_url  = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.source_name
