6310142210073102c398ccb66706d0a0bb273b7c
LorenaMendes <lorenamendes.contato@gmail.com>
2020-05-26 16:27:52 -0300

    models and forms for new parameters added

--- main/forms.py
+++ main/forms.py
@@ -2,37 +2,45 @@ from django import forms
 from .models import CrawlRequest
 
 # class CrawlerSetup(forms.Form):
-#     source_name = forms.CharField(label="Source Name", max_length=200,
-#         widget=forms.TextInput(attrs={'placeholder': 'Example'})
-#     )
-#     base_url = forms.CharField(label="Base URL", max_length=200,
-#        widget=forms.TextInput(attrs={'placeholder': 'www.example.com/data/'})
-#     )
 #     start_date = birth_date = forms.DateField(
 #         widget=forms.TextInput(attrs={'type': 'date'})
 #     )
 #     end_date = birth_date = forms.DateField(
 #         widget=forms.TextInput(attrs={'type': 'date'})
 #     )
-#     captcha_type = forms.ChoiceField(choices = ((1, 'None'), (2, 'Image'), (3, 'Sound')))
-#     antiblock = forms.ChoiceField(choices = (
-#         (1, 'None'),
-#         (2, 'IP rotation'),
-#         (3, 'User-agent rotation'),
-#         (4, 'Random delays'),
-#         (5, 'Use proxy'),
-#         (6, 'Use cookies'),
-#     ))
 #     max_reqs_per_sec = forms.IntegerField(label='Max requisitions per sec', min_value=1, widget=forms.TextInput(attrs={'placeholder': '2'}))
 
 class CrawlRequestForm(forms.ModelForm):
     class Meta:
         model = CrawlRequest
+
+        # Options for IP rotation
+        ip_type = forms.ChoiceField(required=False)
+        tor_password = forms.CharField(required=False)
+        proxy_list = forms.FileField(required=False)
+        max_reqs_per_ip = forms.IntegerField(required=False)
+        max_reuse_rounds = forms.IntegerField(required=False)
+        
+        # Options for User Agent rotation
+        reqs_per_user_agent = forms.IntegerField(required=False)
+        user_agents_file = forms.FileField(required=False)
+        
+        # Options for Delay
+        delay_secs = forms.IntegerField(required=False)
+
         fields = [
             'source_name',
             'base_url',
             'antiblock',
             'captcha',
+            'ip_type',
+            'tor_password',
+            'max_reqs_per_ip',
+            'max_reuse_rounds',
+            'proxy_list',
+            'reqs_per_user_agent',
+            'user_agents_file',
+            'delay_secs',
         ]
 
 class RawCrawlRequestForm(forms.Form):
@@ -54,4 +62,23 @@ class RawCrawlRequestForm(forms.Form):
         ('2', 'Image'),
         ('3', 'Sound'),
     ))
+    
+    # Options for IP rotation
+    ip_type = forms.ChoiceField(required=False, choices = (
+        ('tor', 'Tor'), 
+        ('proxy', 'Proxy'),
+    ))
+    tor_password = forms.CharField(required=False, label="Tor Password", max_length=20,
+        widget=forms.TextInput(attrs={'placeholder': 'Password'})
+    )
+    proxy_list = forms.FileField(required=False, label="Proxy List")
+    max_reqs_per_ip = forms.IntegerField(required=False, label="Max Requisitions per IP")
+    max_reuse_rounds = forms.IntegerField(required=False, label="Max Reuse Rounds")
+    
+    # Options for User Agent rotation
+    reqs_per_user_agent = forms.IntegerField(required=False, label="Requests per User Agent")
+    user_agents_file = forms.FileField(required=False, label="User Agents File")
+    delay_secs = forms.IntegerField(required=False, label="Delay in Seconds")
 
+    # Options for Delay
+    delay_secs = forms.IntegerField(required=False, label="Delay in Seconds")
--- main/models.py
+++ main/models.py
@@ -12,15 +12,36 @@ class CrawlRequest(models.Model):
         ('delay', 'Delays'),
         ('cookies', 'Use cookies'),
     ]
-
+    IP_TYPE = [
+        ('tor', 'Tor'),
+        ('proxy', 'Proxy'),
+    ]
     CAPTCHA_TYPE = [
         ('1', 'None'), 
         ('2', 'Image'),
         ('3', 'Sound'),
     ]
 
-    antiblock = models.CharField(max_length=15, choices=ANTIBLOCK_TYPE, default='1')
+    antiblock = models.CharField(max_length=15, choices=ANTIBLOCK_TYPE, default='none')
+    
+    # Options for antiblock
+        # Options for IP rotation
+    ip_type = models.CharField(max_length=15, choices=IP_TYPE, null=True, blank=True)
+    tor_password = models.CharField(max_length=20, blank=True) # available for Tor
+    proxy_list = models.FileField(max_length=20, blank=True, upload_to=None) # available for Proxy List
+    max_reqs_per_ip = models.IntegerField(blank=True)
+    max_reuse_rounds = models.IntegerField(blank=True)
+
+        # Options for User Agent rotation 
+    reqs_per_user_agent = models.IntegerField(blank=True)
+    user_agents_file = models.FileField(max_length=20, blank=True, upload_to=None) # available for Proxy List
+    
+        # Options for Delay
+    delay_secs = models.IntegerField(blank=True)
+
     captcha = models.CharField(max_length=15, choices=CAPTCHA_TYPE, default='1')
+    # steps = models.TextField(null=True, blank=True)
+
 
     def __str__(self):
         return self.source_name
