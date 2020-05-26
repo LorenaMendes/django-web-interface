e7069fb6d66d5065dea816f544c7def26cc92e6b
LorenaMendes <lorenamendes.contato@gmail.com>
2020-05-26 16:28:58 -0300

    test fixes

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("crawlers/", views.create_crawl, name="create_crawl"),
-    path("steps/", views.create_steps, name="create_steps"),
+    path("crawlers/steps/", views.create_steps, name="create_steps"),
     path("monitoring/", views.monitoring, name="monitoring"),
 ]
--- main/views.py
+++ main/views.py
@@ -13,24 +13,34 @@ def monitoring(response):
 def create_crawl(response):
     context = {}
     if response.method == "POST":
-        print('++++++++++++++++++++++++++++++++++')
-        print(response.POST)
-        print('++++++++++++++++++++++++++++++++++')
         my_form = RawCrawlRequestForm(response.POST)
+        print("FOI 1")
         if my_form.is_valid():
-            print (my_form.cleaned_data)
+            print("FOI 2")
+            print(response.POST)
+            
             new_crawl = CrawlRequestForm(my_form.cleaned_data)
             new_crawl.save()
+
+            objs = CrawlRequest.objects
+            my_obj = objs.filter(base_url = my_form.cleaned_data['base_url'])
+            
             context['url'] = my_form.cleaned_data['base_url']
+            context['id'] = my_obj.last().id
             
             return render(response, "main/steps_creation.html", context)
     else:
         my_form = RawCrawlRequestForm()
     
     context['form'] = my_form
-
     return render(response, "main/create_crawl.html", context)
 
 def create_steps(response):
+    
+    print('################################3')
+    print(response)
+    print('################################3')
+    print("Estou no create_steps")
+    print('################################3')
     
     return render(response, "main/steps_creation.html", {})
