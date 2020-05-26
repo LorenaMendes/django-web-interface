3786dfdbca7e4a64de81d1e9f83c9cd25d66253b
LorenaMendes <lorenamendes.contato@gmail.com>
2020-05-26 16:26:30 -0300

    database changed to mongodb

--- interface/settings.py
+++ interface/settings.py
@@ -77,13 +77,22 @@ WSGI_APPLICATION = 'interface.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'CrawlRequestDB',
+        'HOST': 'localhost',
+        'USER': 'lorena',
+        'PASSWORD': 'myhorseisamazing',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
