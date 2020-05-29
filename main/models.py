9489abfd3e8c0033b69357d4fa49a815dcc89171
LorenaMendes <lorenamendes.contato@gmail.com>
2020-05-29 10:44:03 -0300

    null fields added

--- main/models.py
+++ main/models.py
@@ -34,32 +34,32 @@ class CrawlRequest(models.Model):
     # Options for antiblock
         # Options for IP rotation
     ip_type = models.CharField(max_length=15, choices=IP_TYPE, null=True, blank=True)
-    tor_password = models.CharField(max_length=20, blank=True) # available for Tor
-    proxy_list = models.FileField(max_length=20, blank=True, upload_to=None) # available for Proxy List
-    max_reqs_per_ip = models.IntegerField(blank=True)
-    max_reuse_rounds = models.IntegerField(blank=True)
+    tor_password = models.CharField(max_length=20, blank=True, null=True) # available for Tor
+    proxy_list = models.FileField(max_length=20, blank=True, null=True, upload_to=None) # available for Proxy List
+    max_reqs_per_ip = models.IntegerField(blank=True, null=True)
+    max_reuse_rounds = models.IntegerField(blank=True, null=True)
 
         # Options for User Agent rotation 
-    reqs_per_user_agent = models.IntegerField(blank=True)
+    reqs_per_user_agent = models.IntegerField(blank=True, null=True)
     user_agents_file = models.FileField(max_length=20, blank=True, upload_to=None)
     
         # Options for Delay
-    delay_secs = models.IntegerField(blank=True)
+    delay_secs = models.IntegerField(blank=True, null=True)
     delay_type = models.CharField(max_length=15, choices=DELAY_TYPE, null=True, blank=True)
         
         # Options for Cookies
-    cookies_file = models.FileField(max_length=20, blank=True, upload_to=None)
-    persist_cookies = models.BooleanField(blank=True)
+    cookies_file = models.FileField(max_length=20, blank=True, null=True, upload_to=None)
+    persist_cookies = models.BooleanField(blank=True, null=True)
 
     captcha = models.CharField(max_length=15, choices=CAPTCHA_TYPE, default='1')
 
     # Options for captcha
         # Options for image
-    img_xpath = models.CharField(max_length=100, blank=True)
-    img_url = models.CharField(max_length=100, blank=True)
+    img_xpath = models.CharField(max_length=100, blank=True, null=True)
+    img_url = models.CharField(max_length=100, blank=True, null=True)
         # Options for sound
-    sound_xpath = models.CharField(max_length=100, blank=True)
-    sound_url = models.CharField(max_length=100, blank=True)
+    sound_xpath = models.CharField(max_length=100, blank=True, null=True)
+    sound_url = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.source_name
