a3f0f48c537fddee765864ebfd6a506949b1eab0
gabrielcrds <gabriel.d.a.cardoso@gmail.com>
2020-06-24 15:04:04 -0300

    Stop to try to check instance state by log tail

--- main/staticfiles/js/details.js
+++ main/staticfiles/js/details.js
@@ -1,39 +1,13 @@
 document.addEventListener('DOMContentLoaded',
     function () {
-        var crawler_id = document.getElementById("crawler_id").innerText.trim();
         var instance_id = document.getElementById("last_instance_id").innerText.trim();
         var last_as_running = document.getElementById("instance_running").innerText.trim() == "True";
-        console.log(instance_id != "None" && last_as_running);
-
-        if(instance_id != "None" && last_as_running){
-            document.getElementById("stderr_tail").innerText = "";
+        
+        if(instance_id != "None")
             tail_logs(instance_id);
-            console.log("setting times")
-            setTimeout(() => {
-                console.log("last as running. Checking if still running...");
-                var text = document.getElementById("stderr_tail").innerText.trim();
-                console.log("last log: " + text);
 
-                setTimeout(() => {
-                    console.log("10s passed, checking log again...");
-                    tail_logs(instance_id);
-                    console.log("new log: " + document.getElementById("stderr_tail").innerText.trim());
-                    if (text === document.getElementById("stderr_tail").innerText.trim()) {
-                        console.log("It is not running anymore.");
-                        set_instance_to_not_running(crawler_id, instance_id);
-                    }
-                    else {
-                        console.log("It is still running");
-                        set_to_running(crawler_id, instance_id);
-                        tail_f_logs(instance_id);
-                    }
-                }, 60000);
-            }, 30000);
-        }
-        else{
-            set_to_not_running(crawler_id);
-            if (instance_id != "None") tail_logs(instance_id);
-        }
+        if(last_as_running)
+            tail_f_logs(instance_id);
     },
     false
 );
@@ -44,14 +18,18 @@ function tail_logs(instance_id){
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var response = JSON.parse(this.responseText);
-            if(response["out"].lenght == 0){
+
+            if(response["out"].lenght != 0)
                 document.getElementById("stdout_tail").innerText = response["out"]
-            }
-            else{
+            else
                 document.getElementById("stdout_tail").innerText = "Empty file."
-            }
+
+            if (response["out"].lenght != 0)
+                document.getElementById("stderr_tail").innerText = response["err"]
+            else
+                document.getElementById("stderr_tail").innerText = "Empty file."
+
             document.getElementById("stdout_tail_update").innerText = "last update: " + response["time"]
-            document.getElementById("stderr_tail").innerText = response["err"]
             document.getElementById("stderr_tail_update").innerText = "last update: " + response["time"]
         }
     };
@@ -60,34 +38,9 @@ function tail_logs(instance_id){
 }
 
 function tail_f_logs(instance_id){
-    console.log("tail_f_logs");
     // Calls tail_logs every 5 seconds
-    // setInterval(
-    //     function(){ tail_logs(instance_id);},
-    //     5000
-    // ); 
-}
-
-function set_instance_to_not_running(crawler_id, instance_id){
-    window.location.replace("/detail/stop_crawl/" + crawler_id + "/" + instance_id);
-}
-
-function set_to_not_running(crawler_id){
-    document.getElementById("status_badge").innerHTML = `
-        Status: <span class="badge badge-warning">Not running</span>    
-    `;
-    document.getElementById("run_button").innerHTML = "Run";
-    document.getElementById("run_button").setAttribute("href", "/detail/run_crawl/" + crawler_id);
-    document.getElementById("stop_button").classList.add("disabled");
-    document.getElementById("stop_button").innerHTML = "Stop";
-} 
-
-function set_to_running(crawler_id, instance_id){
-    document.getElementById("status_badge").innerHTML = `
-        Status: <span class="badge badge-success">Running</span>    
-    `;
-    document.getElementById("run_button").innerHTML = "Run";
-    document.getElementById("run_button").classList.add("disabled");
-    document.getElementById("stop_button").innerHTML = "Stop";
-    document.getElementById("stop_button").setAttribute("href", "/detail/stop_crawl/" + crawler_id + "/" + instance_id);
+    setInterval(
+        function(){ tail_logs(instance_id);},
+        5000
+    ); 
 }
