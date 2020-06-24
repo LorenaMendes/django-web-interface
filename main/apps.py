43e35c499c424409bdef25821e2300dfbed77093
gabrielcrds <gabriel.d.a.cardoso@gmail.com>
2020-06-24 15:02:53 -0300

    Set instances 'running' to 'not running' at startup

--- main/apps.py
+++ main/apps.py
@@ -3,3 +3,12 @@ from django.apps import AppConfig
 
 class MainConfig(AppConfig):
     name = 'main'
+
+    def ready(self):
+        # have to import here, after everything is ready
+        from .models import CrawlerInstance
+        instances = CrawlerInstance.objects.filter(running=True)
+        
+        for instance in instances:
+            instance.running = False
+            instance.save()
